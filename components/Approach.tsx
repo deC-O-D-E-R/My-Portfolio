cfb8c9769fc9e33525863f97415e18676364ef9a
Mohd Saif <mohdsaif9990197398@gmail.com>
2024-07-16 15:51:34 +0530

    photos Modified

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -94,7 +94,7 @@ const Card = ({
                 <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
                     {icon}
                 </div>
-                <h2 className="text-3xl text-center pb-[30px] dark:text-white opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+                <h2 className="text-3xl text-center pb-[10px] dark:text-white opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-0  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
                     {title}
                 </h2>
                     <p
